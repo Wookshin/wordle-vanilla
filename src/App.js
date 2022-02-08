ffd1c8b7323837a3335b5e0182b34ec870b385f7
wookshin <wookshins@gmail.com>
2022-02-08 23:33:40 +0900

    키보드 입력 & BACKSPACE 기능

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GameGrid from "./GameGrid.js"
 import GameKeyboard from "./GameKeyboard.js"
 
 class App {
-  state = {x:0, y:0, value:null};
+  state = {x:-1, y:0, value:null, words:Array(6).fill(''), isUpdate: false};
 
   constructor(target) {
     this.target = target;
@@ -11,7 +11,50 @@ class App {
     this.gameGrid = new GameGrid({target});
     this.gameKeyboard = new GameKeyboard({target});
     window.addEventListener('keydown', e => {
-      this.setState({...this.state, value:e.key});
+      let value = e.key.toUpperCase();
+      console.log(value);
+      const regex = /^[A-Z]|BACKSPACE$/;
+      if (!regex.test(value)) {
+        return;
+      }
+
+      let stateX = this.state.x;
+      let stateY = this.state.y; 
+      let x;
+      let y;
+      let words = this.state.words.slice();
+      let isUpdate = true;
+
+      if (value === 'BACKSPACE') {
+        console.log('1',this.state);
+        
+        if (stateX === -1 && stateY === 0) {
+          return;
+        }
+
+        words[stateY] = words[stateY].substr(0, words[stateY].length-1);
+        this.setState({x: stateX, y: stateY, value: '', words, isUpdate});
+
+        if (stateX === 0 && stateY === 0) {
+          x = -1;
+          y = 0;
+          isUpdate = false;
+        }
+        else {
+          x = stateX === 0 ? 4 : stateX - 1;
+          y = stateX === 0 ? stateY - 1 : stateY;
+        }
+        console.log('2',this.state);
+        this.setState({...this.state, x, y, value: words[y][x], isUpdate});
+        console.log('3',this.state);
+      }
+      else {
+        x = stateX === 4 ? 0 : stateX + 1;
+        y = stateX === 4 ? stateY + 1 : stateY;
+        words[y] = words[y] + value;
+        this.setState({x, y, value, words, isUpdate});
+        console.log(this.state);
+      }
     })
   }
   
--- src/GridColumn.js
+++ src/GridColumn.js
@@ -2,10 +2,10 @@ export class GridColumn {
   props = {};
 
   constructor({target}) {
-    const gridColumn = document.createElement('div');
-    this.gridColumn = gridColumn;
-    gridColumn.classList.add('grid__column');
-    target.appendChild(gridColumn);
+    const $gridColumn = document.createElement('div');
+    this.$gridColumn = $gridColumn;
+    $gridColumn.classList.add('grid__column');
+    target.appendChild($gridColumn);
   }
 
   setProps(nextProps) {
@@ -14,6 +14,6 @@ export class GridColumn {
   }
 
   render() {
-    this.gridColumn.textContent = this.props.value;
+    this.$gridColumn.textContent = this.props.value;
   }
 }
