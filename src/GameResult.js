d818da4e722068dfb78bd18e3ca55fc24a6b1dbc
wookshin <wookshins@gmail.com>
2022-02-09 20:57:01 +0900

    proxy 추가 & 정답 확인

---
+++ src/GameResult.js
@@ -0,0 +1,30 @@
+class GameResult {
+  props = {};
+
+  constructor({target}) {
+    const $gameResult = document.createElement('div');
+    this.$gameResult = $gameResult;
+    $gameResult.classList.add('game__result');
+    target.appendChild($gameResult);
+  }
+
+  setProps(nextProps) {
+    this.props = {...this.props, ...nextProps};
+    this.render();
+  }
+
+  render() {
+    const { answer, isSuccess} = this.props;
+    if (isSuccess === null) {
+      return;
+    }
+    this.$gameResult.innerHTML = `
+      <div>정답은 <span class="result__answer">${answer}</span> 입니다!</div>
+      <div>
+      ${isSuccess? '축하드립니다!🎉' : '다음 기회에 맞춰주세요!😉'}
+      </div>
+    `
+  }
+}
+
+export default GameResult;
--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
+const HEROKU_POINT = 'https://cors-anywhere.herokuapp.com/'
 const API_END_POINT = 'https://ac-dict.naver.com/enko/ac?st=11&r_lt=11&q=';
 const request = async (word) => {
   try {
-    const res = await fetch(`${API_END_POINT}{word}`);
+    const res = await fetch(`${HEROKU_POINT}${API_END_POINT}${word}`);
 
     if (!res.ok) {
       throw new Error('서버의 상태가 이상합니다!');
